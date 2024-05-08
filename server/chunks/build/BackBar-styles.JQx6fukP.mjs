1a4b253a046a7a91f0ac91d029a6ea044ffa8770
bertakang <bertakang@gmail.com>
2024-05-08 07:52:18 -0700

    Delete server directory

--- server/chunks/build/BackBar-styles.JQx6fukP.mjs
+++
@@ -1,6 +0,0 @@
-const BackBar_vue_vue_type_style_index_0_scoped_23197dc4_lang = ".wrapper[data-v-23197dc4]{align-items:center;background:linear-gradient(90deg,#aa5486,#7e315d);border-bottom:.5px solid #fed1eb;display:flex;height:100px;padding:0 32px;position:fixed;top:0;width:100%;z-index:199999999999}";
-
-const BackBarStyles_JQx6fukP = [BackBar_vue_vue_type_style_index_0_scoped_23197dc4_lang];
-
-export { BackBarStyles_JQx6fukP as default };
-//# sourceMappingURL=BackBar-styles.JQx6fukP.mjs.map
--- server/chunks/build/_recipe_-styles.UgOuxJ_8.mjs
+++
@@ -1,6 +0,0 @@
-const _recipe__vue_vue_type_style_index_0_scoped_21141a46_lang = "h2[data-v-21141a46]{font-size:22px}h2[data-v-21141a46],h3[data-v-21141a46]{color:#fed1eb;margin:0}h3[data-v-21141a46]{text-transform:uppercase}p[data-v-21141a46],ul[data-v-21141a46]{margin:0}ul[data-v-21141a46]{list-style-type:circle;padding-left:32px}li[data-v-21141a46]{color:#fed1eb;font-family:Archivo,sans-serif;padding-left:16px}li[data-v-21141a46],ol[data-v-21141a46]{margin:0}ol[data-v-21141a46]{list-style-type:decimal}.recipes-page[data-v-21141a46]{padding:32px}.recipes-description[data-v-21141a46]{margin-top:100px;position:static}.recipe-ingredients[data-v-21141a46]{margin:16px 0}.recipe-directions[data-v-21141a46]{margin-top:16px}";
-
-const _recipe_Styles_UgOuxJ_8 = [_recipe__vue_vue_type_style_index_0_scoped_21141a46_lang];
-
-export { _recipe_Styles_UgOuxJ_8 as default };
-//# sourceMappingURL=_recipe_-styles.UgOuxJ_8.mjs.map
